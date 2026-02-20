91359f32fc5b53cb725961a6450a619ccf8af5d3
Clément <81437241+clementmueller@users.noreply.github.com>
2026-02-20 18:55:46 +0100

    v2.1: wake lock

--- app/src/main/java/com/example/mediaxmanager/MainActivity.kt
+++ app/src/main/java/com/example/mediaxmanager/MainActivity.kt
@@ -10,14 +10,15 @@ import com.example.mediaxmanager.ui.theme.MediaControllerTheme
 import android.provider.Settings
 import android.content.Intent
 import androidx.core.app.NotificationManagerCompat
+import android.view.WindowManager
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MediaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) // ← add this
 
-        // Redirect to settings if permission not granted
         if (!isNotificationListenerEnabled()) {
             startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
         }
